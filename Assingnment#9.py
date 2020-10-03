2710657e502eb78b19b9682fbece7e1deb8e01c6
Carmona Elias <70917686+Carmona-Elias@users.noreply.github.com>
2020-10-03 23:41:11 +0200

    Add files via upload

--- Assingnment#9.py
+++ Assingnment#9.py
@@ -1,11 +1,11 @@
 class Vehicle:
-    def __init__(self, make, model, year, weight, needsMaintenance = False, tripsSinceMaintenance = 0):
+    def __init__(self, make, model, year, weight):
         self.Make = make
         self.Model = model
         self.Year = year
         self.Weight = weight
-        self.NeedsMaintenance = needsMaintenance
-        self.TripsSinceMaintenance = tripsSinceMaintenance
+        self.NeedsMaintenance = False
+        self.TripsSinceMaintenance = 0
 
 
     # Setters
@@ -23,21 +23,34 @@ class Vehicle:
 
     def setWeight(self, weight):
 
-        self.weight = weight
+        self.Weight = weight
 
-    def setNeedM(self, needMaintenance):
 
-        self.NeedsMaintenance = needMaintenance
+    # Getters
+    def getMake(self):
+        return self.Make
 
-    def setTripsM(self, tripsMaintenance):
+    def getModel(self):
+        return self.Model
 
-        self.TripsSinceMaintenance = tripsMaintenance
+    def getYear(self):
+        return self.Year
+
+    def getWeight(self):
+        return self.Weight
+
+    def getNeedsMaintenance(self):
+        return self.NeedsMaintenance
+
+    def getTripsSinceMaintenance(self):
+        return self.TripsSinceMaintenance
+
+
 
 
 class Cars(Vehicle):
-    def __init__(self, make, model, year, weight, needsMaintenance = False , tripsSinceMaintenance = 0):
-        Vehicle.__init__(self, make, model, year, weight, needsMaintenance, tripsSinceMaintenance)
-
+    def __init__(self, make, model, year, weight):
+        Vehicle.__init__(self, make, model, year, weight)
 
     def Drive(self):
 
@@ -52,6 +65,7 @@ class Cars(Vehicle):
         if self.isDriving == False:
             self.TripsSinceMaintenance += 1
 
+
     def maintenance(self):
 
         if self.TripsSinceMaintenance > 100:
@@ -62,8 +76,8 @@ class Cars(Vehicle):
         self.NeedsMaintenance = False
 
 class Planes(Vehicle):
-    def __init__(self, make, model, year, weight, needsMaintenance = False, tripsSinceMaintenance = 0):
-        Vehicle.__init__(self, make, model, year, weight, needsMaintenance, tripsSinceMaintenance)
+    def __init__(self, make, model, year, weight):
+        Vehicle.__init__(self, make, model, year, weight)
 
     def fly(self):
 
@@ -81,44 +95,3 @@ class Planes(Vehicle):
 
     def __str__(self):
         return ('The Plane cannot fly until its is repaired')
-
-
-car1 = Cars('Mercedes', 'Compressor', 2002, 3500)
-car1.Drive()
-car1.Drive()
-car1.Drive()
-car1.Drive()
-car1.Drive()
-print(car1.Make)
-print(car1.Model)
-print(car1.Year)
-print(car1.Weight)
-print(car1.NeedsMaintenance)
-print(car1.TripsSinceMaintenance)
-car2 = Cars('BMW', 'X5', 2012, 7450)
-car2.Drive()
-car2.Drive()
-car2.Drive()
-
-print(car2.Make)
-print(car2.Model)
-print(car2.Year)
-print(car2.Weight)
-print(car2.NeedsMaintenance)
-print(car2.TripsSinceMaintenance)
-
-car3 = Cars('Ford', 'Wild Track', 2015, 8900)
-car3.Drive()
-car3.Drive()
-car3.Drive()
-car3.Drive()
-
-print(car3.Make)
-print(car3.Model)
-print(car3.Year)
-print(car3.Weight)
-print(car3.NeedsMaintenance)
-print(car3.TripsSinceMaintenance)
-
-
-
